42a23d2589556811943ceace9edb6be440ee01fa
dualshockmaster <>
2019-12-06 10:35:35 -0300

    CORS Middleware for Laravel 5/6

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,7 @@ use Illuminate\Http\Request;
 
 Route::post('register', 'Api\UserController@register');
 Route::post('login', 'Api\UserController@authenticate');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('products', 'Api\ProductController@index');
+});
